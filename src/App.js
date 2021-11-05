a56b88a5c39c837893bd604e4aa3b26c2bfcfce5
Rafael <58118643+RafaelNassybullin@users.noreply.github.com>
2021-11-05 20:23:03 +0300

    gh-page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import Particles from 'react-tsparticles'
+import './index.css'
+import r1 from './img/java&scripts.png'
+import r2 from './img/angularLogo.png'
+import r3 from './img/react.png'
+import r4 from './img/typescript.jpg'
+import r5 from './img/vue.webp'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={'ha'}>
+            <Particles
+                params={{
+                    "particles": {
+                        "number": {
+                            "value": 23,
+                            "density": {
+                                "enable": true,
+                                "value_area": 800
+                            }
+                        },
+                        "line_linked": {
+                            "enable": false
+                        },
+                        "move": {
+                            "enable": true,
+                            "speed": 2.5,
+                            "direction": "none",
+                            "random": false,
+                            "straight": false,
+                            "out_mode": "out",//bounce
+                            "bounce": false,
+                            "attract": {
+                                "enable": false,
+                                "rotateX": 600,
+                                "rotateY": 1200
+                            }
+                        },
+                        "shape": {
+                            "type": [
+                                "image"
+                            ],
+                            "image": [
+                                {
+                                    "src": `${r1}`,
+                                    "height": 48,
+                                    "width": 48
+                                },
+                                {
+                                    "src": `${r2}`,
+                                    "height": 68,
+                                    "width": 68
+                                },
+                                {
+                                    "src": `${r3}`,
+                                    "height": 48,
+                                    "width": 53
+                                },
+                                {
+                                    "src": `${r4}`,
+                                    "height": 48,
+                                    "width": 48
+                                },
+                                {
+                                    "src": `${r5}`,
+                                    "height": 48,
+                                    "width": 48
+                                },
+
+                            ]
+                        },
+                        "color": {
+                            "value": "#CCC"
+                        },
+                        "size": {
+                            "value": 30,
+                            "random": false,
+                            "anim": {
+                                "enable": true,
+                                "speed": .4,
+                                "size_min": 26,
+                                "sync": false
+                            }
+                        }
+                    },
+                    "retina_detect": true
+                }}
+            />
+        </div>
+    );
 }
 
 export default App;
